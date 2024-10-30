5239c042a7c4610f64e3b1a9fa8a63757c7cf50d
Pawissanutt Lertpongrujikorn <pawissanutt@gmail.com>
2024-10-30 02:40:08 -0500

    Implement basic invoking and data loading

---
+++ examples/helloworld/helloworld/__init__.py
@@ -0,0 +1,89 @@
+import asyncio
+import json
+from importlib.metadata import metadata
+
+from fastapi import FastAPI
+from tsidpy import TSID
+
+from oaas_sdk2_py import BaseObject, Oparaca, start_grpc_server, InvocationRequest, InvocationResponse
+from oaas_sdk2_py.engine import InvocationContext
+from oaas_sdk2_py.model import ObjectMeta
+from oaas_sdk2_py.pb.oprc import ResponseStatus
+
+oaas = Oparaca(odgm_url="http://localhost:10001")
+greeter = oaas.new_cls(pkg="example", name="hello")
+
+
+@greeter
+class Greeter(BaseObject):
+    def __init__(self, meta: ObjectMeta = None, ctx: InvocationContext = None):
+        super().__init__(meta, ctx)
+
+    @greeter.data_getter(index=0)
+    async def get_intro(self, raw: bytes=None) -> str:
+        return raw.decode("utf-8")
+
+
+    @greeter.data_setter(index=0)
+    async def set_intro(self, data: str) -> bytes:
+        return data.encode("utf-8")
+
+
+    @greeter.func(stateless=True)
+    async def new(self, req: InvocationRequest):
+        if len(req.payload) == 0:
+            await self.set_intro("How are you?")
+        else:
+            payloads = json.loads(req.payload)
+            await self.set_intro(payloads.get("intro", "How are you?"))
+        tsid = TSID(self.meta.obj_id)
+        resp = f'{{"id":{self.meta.obj_id},"tsid":"{tsid.to_string()}"}}'
+        return InvocationResponse(
+            status=ResponseStatus.OKAY,
+            payload=resp.encode()
+        )
+
+    @greeter.func()
+    async def greet(self,  req: InvocationRequest):
+        if len(req.payload) == 0:
+            name = "world"
+        else:
+            payloads = json.loads(req.payload)
+            name = payloads.get("name", "world")
+        intro = await self.get_intro()
+        resp = "hello " + name + ". " + intro
+        return InvocationResponse(
+            status=ResponseStatus.OKAY,
+            payload=resp.encode()
+        )
+
+    # @greeter.func()
+    # async def talk(self, friend_id: int):
+    #     friend = self.ctx.create_object_from_ref(greeter, friend_id)
+    #     # REMOTE RPC
+    #     friend.greet()
+
+    @greeter.func()
+    async def change_intro(self, req: InvocationRequest):
+        if len(req.payload) > 0:
+            payloads = json.loads(req.payload)
+            await self.set_intro(payloads.get("intro", "How are you?"))
+        return InvocationResponse(
+            status=ResponseStatus.OKAY
+        )
+
+
+app = FastAPI()
+router = oaas.build_router()
+app.include_router(router)
+
+
+async def main(port=8080):
+    server = await start_grpc_server(oaas, port=port)
+    print(f'Serving on {port}')
+    await server.wait_closed()
+
+
+if __name__ == '__main__':
+    # print(json.dumps(oaas.meta_repo.export_pkg(), indent=2))
+    asyncio.run(main())
---
+++ justfile
@@ -0,0 +1,9 @@
+
+
+protoc:
+    poetry run python -m grpc_tools.protoc \
+      -Ioaas_sdk2_py/pb=./protos \
+      --python_betterproto_out=oaas_sdk2_py/pb \
+      --python_betterproto_opt=pydantic_dataclasses,typing.310 \
+      ./protos/oprc-data.proto \
+      ./protos/oprc-invoke.proto
